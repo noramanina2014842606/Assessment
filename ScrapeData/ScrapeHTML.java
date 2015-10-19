2c56d99a477437a7b5280cdd7ba4ea7e539151b0
noramanina2014842606 <ieyna910@gmail.com>
2015-10-19 15:22:59 +0800

    Scrape data

--- ScrapeData/ScrapeHTML.java
+++ ScrapeData/ScrapeHTML.java
@@ -3,44 +3,45 @@ import org.jsoup.helper.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import java.io.*;
 
 import java.io.IOException;
-
-
-import java.io.*; // Only needed if scraping a local File.
-
-import java.io.IOException;
+import java.util.*;
  
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
  
-public class ScrapeHTML{
+public class ScrapeHTML
+{
  
   public static void main(String[] args) throws IOException {
-    try {
-      // fetch the document over HTTP
-      Document doc = Jsoup.connect("http://fskm.uitm.edu.my/v1/fakulti/staff-directory/academic/1097.html").get();
+  
+      Document doc = null;
+      String title = null;
+     
+      try 
+      {
+        doc = Jsoup.connect("http://fskm.uitm.edu.my/v1/fakulti/staff-directory/academic/1097.html").get();
+        title = doc.title();
+        System.out.println("title: " + title);
       
-      // get the page title
-      String title = doc.title();
-      System.out.println("title: " + title);
-      
-      // get all links in page
-      Elements links = doc.select("a[href]");
-      for (Element link : links) {
-        // get the value from the href attribute
-        System.out.println("\nlink: " + link.attr("href"));
-        System.out.println("text: " + link.text());
+     
+      Element tables = doc.getElementById("mytable");
+      Elements rows = tables.getElementsByTag("tr");
+      for (Element row : rows){
+          Elements tds = row.getElementsByTag("td");
+          for (int i = 0; i < tds.size(); i++) {
+                    if (i == 1) System.out.println(tds.get(i).text());
+                }
       }
-    } catch (IOException e) {
-    e.printStackTrace();
+        
+            }catch (java.io.IOException ex) {
+            System.out.println("IO Error: " + ex);
+        }  
+    
+       
     }
-  }
-}
+            }  
+  
